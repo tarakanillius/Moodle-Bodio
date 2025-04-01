61a278df4a182ca1dd9a16585364ebab29fd2e20
Diego <diegorosa17527@hotmail.com>
2025-04-01 11:24:35 +0200

    Added quiz page to App.js

--- project-root/frontend/src/App.js
+++ project-root/frontend/src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
-import Login from "./pages/Login"; // Assicurati che il percorso sia corretto
+import Login from "./pages/Login";
+import Quiz from "./pages/Quiz"; // Assicurati che il percorso sia corretto
 
 function App() {
   return (
       <Router>
         <Routes>
-          {/* La pagina di login sarà la prima visualizzata */}
-          <Route path="/" element={<Navigate to="/login" />} />
-          <Route path="/login" element={<Login />} />
+            {/* La pagina di login sarà la prima visualizzata */}
+            <Route path="/" element={<Navigate to="/login" />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/quiz" element={<Quiz />} />
         </Routes>
       </Router>
   );
