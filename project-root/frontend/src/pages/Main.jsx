f7a22d255c0f4ad81caa88613aa694256db2dbc8
tarakanillius <tarakanillius@proton.me>
2025-04-01 11:04:24 +0200

    added additional account images and sidebar was extracted from Main component

--- project-root/frontend/src/pages/Main.jsx
+++ project-root/frontend/src/pages/Main.jsx
@@ -2,49 +2,15 @@ import React, { useContext } from "react";
 import { GlobalContext } from "../context/GlobalContext";
 import "../styles/main.css";
 import Settings from "./Settings";
+import Sidebar from "../components/Sidebar";
 
 const Main = () => {
-    const { selectedComponent, setSelectedComponent } = useContext(GlobalContext);
-
-    const user = {
-        name: "Mario Rossi",
-        role: "Student",
-        avatar: "/favicon.ico",
-    };
+    const { selectedComponent } = useContext(GlobalContext);
 
     return (
         <div className="main-wrapper">
             {/* Sidebar on the left */}
-            <div className="sidebar">
-                <h1>m0.0dle</h1>
-                <div className="sidebar-buttons">
-                    <button className={selectedComponent === "home" ? "active" : ""} onClick={() => setSelectedComponent("home")}>
-                        Home
-                    </button>
-                    <button className={selectedComponent === "teachers" ? "active" : ""} onClick={() => setSelectedComponent("teachers")}>
-                        Docenti
-                    </button>
-                    <button className={selectedComponent === "students" ? "active" : ""} onClick={() => setSelectedComponent("students")}>
-                        Studenti
-                    </button>
-                    <button className={selectedComponent === "modules" ? "active" : ""} onClick={() => setSelectedComponent("modules")}>
-                        Moduli
-                    </button>
-                    <button className={selectedComponent === "settings" ? "active" : ""} onClick={() => setSelectedComponent("settings")}>
-                        Impostazioni
-                    </button>
-                </div>
-                <div className="sidebar-account">
-                    <div className="account">
-                        <img src={user.avatar} alt="User" className="avatar" />
-                        <div className="account-info">
-                            <span className="name">{user.name}</span>
-                            <span className="role">{user.role}</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
+            <Sidebar/>
             {/* Main content area */}
             <div className="content">
                 {selectedComponent === "home" && <h2>Home Component</h2>}
