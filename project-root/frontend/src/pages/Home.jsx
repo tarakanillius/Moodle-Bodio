4d695ed478f707c206b2ecb4fb5af2c96069461e
Julie <julie.gambale@gmail.com>
2025-04-01 11:14:21 +0200

    fixed school subject components

--- project-root/frontend/src/pages/Home.jsx
+++ project-root/frontend/src/pages/Home.jsx
@@ -5,24 +5,39 @@ const Home = () => {
     return (
         <div className="body">
             <div className="container">
-                <h1>Modulo 320</h1>
-                <p>Programmare in base a un modello orientato agli oggetti</p>
+                <img src="/assets/Antracite.jpeg"/>
+                <div className="content">
+                    <h3>Modulo 320</h3>
+                    <p>Programmare in base a un modello orientato agli oggetti</p>
+                </div>
             </div>
             <div className="container">
-                <h1>Modulo 322</h1>
-                <p>Sviluppare e implementare interfacce grafiche</p>
+                <img src="/assets/Vetro_vulcanico.jpeg"/>
+                <div className="content">
+                    <h3>Modulo 322</h3>
+                    <p>Sviluppare e implementare interfacce grafiche</p>
+                </div>
             </div>
             <div className="container">
-                <h1>Modulo 165</h1>
-                <p>Utilizzare banche dati NoSQL</p>
+                <img src="/assets/Quercia.jpeg"/>
+                <div className="content">
+                    <h3>Modulo 165</h3>
+                    <p>Utilizzare banche dati NoSQL</p>
+                </div>
             </div>
             <div className="container">
-                <h1>Modulo 426</h1>
-                <p>Sviluppare software con metodi agili</p>
+                <img src="/assets/Sabbia.jpeg"/>
+                <div className="content">
+                    <h3>Modulo 426</h3>
+                    <p>Sviluppare software con metodi agili</p>
+                </div>
             </div>
             <div className="container">
-                <h1>Modulo 293</h1>
-                <p>Creare e pubblicare una pagina web</p>
+                <img src="/assets/Tortora.jpeg"/>
+                <div className="content">
+                    <h3>Modulo 293</h3>
+                    <p>Creare e pubblicare una pagina web</p>
+                </div>
             </div>
         </div>
     );
