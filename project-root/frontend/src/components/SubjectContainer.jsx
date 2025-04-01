01516aa7e8a3f0bde40c52fa3a4298c0a53a7aa7
Julie <julie.gambale@gmail.com>
2025-04-01 13:48:59 +0200

    added logo and scrollbar

--- project-root/frontend/src/components/SubjectContainer.jsx
+++ project-root/frontend/src/components/SubjectContainer.jsx
@@ -2,9 +2,6 @@ import React from "react";
 import "../styles/home.css";
 
 export default function Sidebar({image, schoolSubject, description}) {
-    console.log(image);
-    console.log(schoolSubject);
-    console.log(description);
     return (
         <div className="container">
             <img src={image}/>
--- project-root/frontend/src/pages/Home.jsx
+++ project-root/frontend/src/pages/Home.jsx
@@ -2,14 +2,26 @@ import React from "react";
 import "../styles/home.css";
 import SubjectContainer from "../components/SubjectContainer.jsx";
 
-const Home = () => {
+const Home = (event) => {
+    const container = event.target;
+    const scrollAmount = event.deltaY;
+    container.scrollTo({
+        top: 0,
+        left: container.scrollLeft + scrollAmount,
+        behavior: 'smooth'
+    });
     return (
-        <div className="body">
-            <SubjectContainer image={"/assets/Antracite.jpeg"} schoolSubject={"Modulo 320"} description={"Programmare in base a un modello orientato agli oggetti"}/>
-            <SubjectContainer image={"/assets/Vetro_vulcanico.jpeg"} schoolSubject={"Modulo 322"} description={"Sviluppare e implementare interfacce grafiche"}/>
-            <SubjectContainer image={"/assets/Quercia.jpeg"} schoolSubject={"Modulo 165"} description={"Utilizzare banche dati NoSQL"}/>
-            <SubjectContainer image={"/assets/Sabbia.jpeg"} schoolSubject={"Modulo 426"} description={"Sviluppare software con metodi agili"}/>
-            <SubjectContainer image={"/assets/Tortora.jpeg"} schoolSubject={"Modulo 293"} description={"Creare e pubblicare una pagina web"}/>
+        <div className="page-home">
+            <div className="header">
+                <img id="logo_ameti" src="/assets/logo_ameti.jpeg"/>
+            </div>
+            <div className="home-body">
+                <SubjectContainer image={"/assets/Antracite.jpeg"} schoolSubject={"Modulo 320"} description={"Programmare in base a un modello orientato agli oggetti"}/>
+                <SubjectContainer image={"/assets/Vetro_vulcanico.jpeg"} schoolSubject={"Modulo 322"} description={"Sviluppare e implementare interfacce grafiche"}/>
+                <SubjectContainer image={"/assets/Quercia.jpeg"} schoolSubject={"Modulo 165"} description={"Utilizzare banche dati NoSQL"}/>
+                <SubjectContainer image={"/assets/Sabbia.jpeg"} schoolSubject={"Modulo 426"} description={"Sviluppare software con metodi agili"}/>
+                <SubjectContainer image={"/assets/Tortora.jpeg"} schoolSubject={"Modulo 293"} description={"Creare e pubblicare una pagina web"}/>
+            </div>
         </div>
     );
 };
