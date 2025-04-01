4754ce6283dc53379c6127ace512ddb31d4b654e
Julie <julie.gambale@gmail.com>
2025-04-01 12:43:12 +0200

    fixed school subject components

--- project-root/frontend/src/pages/Home.jsx
+++ project-root/frontend/src/pages/Home.jsx
@@ -1,44 +1,15 @@
 import React from "react";
-import style from "../styles/home.css";
+import "../styles/home.css";
+import SubjectContainer from "../components/SubjectContainer.jsx";
 
 const Home = () => {
     return (
         <div className="body">
-            <div className="container">
-                <img src="/assets/Antracite.jpeg"/>
-                <div className="content">
-                    <h3>Modulo 320</h3>
-                    <p>Programmare in base a un modello orientato agli oggetti</p>
-                </div>
-            </div>
-            <div className="container">
-                <img src="/assets/Vetro_vulcanico.jpeg"/>
-                <div className="content">
-                    <h3>Modulo 322</h3>
-                    <p>Sviluppare e implementare interfacce grafiche</p>
-                </div>
-            </div>
-            <div className="container">
-                <img src="/assets/Quercia.jpeg"/>
-                <div className="content">
-                    <h3>Modulo 165</h3>
-                    <p>Utilizzare banche dati NoSQL</p>
-                </div>
-            </div>
-            <div className="container">
-                <img src="/assets/Sabbia.jpeg"/>
-                <div className="content">
-                    <h3>Modulo 426</h3>
-                    <p>Sviluppare software con metodi agili</p>
-                </div>
-            </div>
-            <div className="container">
-                <img src="/assets/Tortora.jpeg"/>
-                <div className="content">
-                    <h3>Modulo 293</h3>
-                    <p>Creare e pubblicare una pagina web</p>
-                </div>
-            </div>
+            <SubjectContainer image={"/assets/Antracite.jpeg"} schoolSubject={"Modulo 320"} description={"Programmare in base a un modello orientato agli oggetti"}/>
+            <SubjectContainer image={"/assets/Vetro_vulcanico.jpeg"} schoolSubject={"Modulo 322"} description={"Sviluppare e implementare interfacce grafiche"}/>
+            <SubjectContainer image={"/assets/Quercia.jpeg"} schoolSubject={"Modulo 165"} description={"Utilizzare banche dati NoSQL"}/>
+            <SubjectContainer image={"/assets/Sabbia.jpeg"} schoolSubject={"Modulo 426"} description={"Sviluppare software con metodi agili"}/>
+            <SubjectContainer image={"/assets/Tortora.jpeg"} schoolSubject={"Modulo 293"} description={"Creare e pubblicare una pagina web"}/>
         </div>
     );
 };
